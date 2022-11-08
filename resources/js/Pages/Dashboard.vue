919100969607faf75265e435d2f3cf655c34de09
MwauraKimani <kimmwaus@gmail.com>
2022-11-09 00:01:12 +0300

    update to log in page

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="dashboard-holder">
+        <div class="side-bar"></div>
+        <div class="main-body">
+            <div class="top-navigation">
+                <div class="top-section">
+
+                </div>
+                <div class="bottom-section">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Dashboard"
+}
+</script>
+
+<style scoped lang="scss" >
+
+
+.dashboard-holder{
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    background-color: #f2f7f8;
+    .side-bar{
+        width: 250px;
+        height: 100vh;
+        background-color: #1f2937;
+        box-shadow: 0 0 6px grey;
+    }
+    .main-body{
+        width: calc(100vw - 260px);
+        margin-left: 5px;
+
+        .top-navigation{
+            width: 100%;
+            border: 1px solid #b4b4b4;
+            background-color: white;
+            border-bottom: none;
+
+            .top-section{
+                width: 100%;
+                height: 56px;
+                border-bottom: 1px solid #b4b4b4;
+            }
+            .bottom-section{
+                display: none;
+                width: 100%;
+                height: 60px;
+                border-bottom: 1px solid #b4b4b4;
+            }
+
+        }
+    }
+}
+
+</style>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,21 +1,97 @@
 <template>
-    <p>{{ cot }}</p>
+    <div class="bg-holder">
+        <div class="log-in-wrapper">
+            <div class="background-effect">
+
+            </div>
+            <form class="login-section">
+                <h1>Log In</h1>
+                <div >
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+<!--                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>-->
+                </div>
+                <div class="mb-[30px]">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+<!--                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>-->
+                </div>
+                <button type="submit" class="btn bg-primary text-white">Submit</button>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
-import { useStore } from "../Pinia/Store";
+import {useStore} from "../Pinia/Store";
 
 export default {
     name: "Welcome",
-    setup(){
+    setup() {
         const store = useStore();
         let cot = store.count;
 
-        return { cot }
+        return {cot}
     }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.bg-holder {
+    width: 100%;
+    height: 100%;
+    background-image: url("./storage/background-image.jpg");
+    align-items: center;
+    justify-content: center;
+    mix-blend-mode: multiply;
+    background-position: center;
+    background-size: cover;
+    overflow: hidden;
 
+
+    .log-in-wrapper {
+        width: 100vw;
+        height: 100vh;
+        background-color: rgba(black,0.6);
+
+
+        .background-effect {
+            width: 100vw;
+            height: 100vh;
+            filter: blur(1px);
+        }
+
+        .login-section {
+            width: 500px;
+            filter: none;
+            height: 430px;
+            background-color: white;
+            border-radius: 8px;
+            box-shadow: 0 0 6px #424242;
+            position: absolute;
+            top: calc((100vh - 430px) / 2);
+            left: calc((100vw - 500px) / 2);
+            h1{
+                font-size:2.5em;
+                padding: 20px;
+                //border-bottom: 2px solid rgb(121, 120, 120);
+                text-align: center;
+            }
+
+            &>div{
+                padding:10px 20px;
+                input{
+                    border-radius: 2px !important;
+                }
+            }
+
+            button{
+                display: block;
+                width: 200px;
+                margin: auto;
+            }
+        }
+    }
+
+}
 </style>
