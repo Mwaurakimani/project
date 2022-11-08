f4cc0ecaffb27c0247070830cb4e4208cb40b98c
MwauraKimani <kimmwaus@gmail.com>
2022-11-08 22:46:05 +0300

    update to pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,19 @@
 import './bootstrap';
 import '../css/app.css';
 
+import jQuery from 'jquery';
+window.$ = jQuery;
+
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createPinia } from "pinia/dist/pinia";
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+const Store = createPinia();
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -16,6 +22,7 @@ createInertiaApp({
         return createApp({ render: () => h(app, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(Store)
             .mount(el);
     },
 });
